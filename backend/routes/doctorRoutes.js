452372fe42c5c14503155bc7cb72859efa5d44ca
Kanishka-garga <samsungkanishka27@gmail.com>
2024-11-02 23:13:10 +0530

    Appointment handling complete

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import Doctor from "../database/UserModels/Doctor.js";
+import checkAuthMiddleware from "../middlewares/auth.js";
 
-const doctorRouter = new express.Router()
+const doctorRouter = new express.Router();
 
-doctorRouter.patch("/doctor/:id/profile", async (req, res) => {
+doctorRouter.patch(
+  "/doctor/me/profile",
+  checkAuthMiddleware(["DOCTOR"]),
+  async (req, res) => {
     // first check credentials: TODO
-    const userId = req.params.id;
-    console.log(userId);
+    const userId = req.user._id;
+    // console.log(userId);
     try {
       const user = await Doctor.findByIdAndUpdate(userId, req.body, {
         new: true,
@@ -15,11 +19,12 @@ doctorRouter.patch("/doctor/:id/profile", async (req, res) => {
       if (!user) {
         return res.status(404).send();
       }
-      res.send(user);
+      res.status(200).send(user);
     } catch (error) {
       return res.status(400).send(error);
     }
     // The profile data sent by the user must be updated in the database
-  });
+  }
+);
 
 export default doctorRouter;
