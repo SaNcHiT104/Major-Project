646e691c0a88009c21db50684274d37026f0a084
Kanishka-garga <samsungkanishka27@gmail.com>
2024-11-04 19:59:31 +0530

    Prior work, and header image added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import WelcomePage from "./Components/authComponent/WelcomePage.jsx";
-import PatientProfile from "./Components/Patient/PatientProfile.jsx";
 export default function App() {
   return (
     <>
-      <>{<WelcomePage />}</>
+      <>{ <WelcomePage /> }</>
     </>
   );
 }
