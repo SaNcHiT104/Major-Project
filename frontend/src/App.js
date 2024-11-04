b65517a3ddc292294d4b0698c68cfefa6f37902f
SaNcHiT104 <jainsanchit14112002@gmail.com>
2024-11-04 13:36:29 +0530

    assets updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import WelcomePage from "./Components/authComponent/WelcomePage.jsx";
+import PatientProfile from "./Components/Patient/PatientProfile.jsx";
 export default function App() {
   return (
     <>
-      <>{ <WelcomePage /> }</>
+      <>{<WelcomePage />}</>
     </>
   );
 }
